11ac935b7aa4ca4082190e2035f3a42915078fad
timstetter <tstetter@gmail.com>
2020-10-15 20:00:29 -0400

    First Commit

---
+++ src/DieRoleSimulation.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class DieRoleSimulation {
+
+	public static void main(String[] args) {
+
+		Scanner scnr = new Scanner(System.in);
+		boolean keepRolling = true;
+
+		System.out.println("Welcome to the Grand Circus Casino!\n");
+		
+		while (keepRolling) {
+			int sidesInput = Validator.getInt(scnr, "Enter the number of sides for the die: ");
+
+			int dieOneValue = generateRandomDieRoll(sidesInput);
+			int dieTwoValue = generateRandomDieRoll(sidesInput);
+
+			System.out.println("You rolled a " + dieOneValue + " & " + dieTwoValue);
+			crapsMessages(dieOneValue, dieTwoValue);
+			
+
+			keepRolling = Validator.getYesNo(scnr, "Roll again? (yes/no)? ");
+		}
+		System.out.println("Thanks for playing!");
+		scnr.close();
+
+	}
+
+	private static int generateRandomDieRoll(int dieRoll) {
+		int die;
+		die = (int) (Math.random() * dieRoll + 1);
+		return die;
+	}
+	private static void crapsMessages(int dieRoll1, int dieRoll2) {
+		if (dieRoll1 == 1 && dieRoll2 == 1) {
+			System.out.println("Snake Eyes!");
+		}else if (dieRoll1 == 6 && dieRoll2 == 6) {
+			System.out.println("Box Cars!");
+		}else if (dieRoll1 + dieRoll2 == 7) {
+			System.out.println("Craps!");
+		}
+	}
+	
+}
